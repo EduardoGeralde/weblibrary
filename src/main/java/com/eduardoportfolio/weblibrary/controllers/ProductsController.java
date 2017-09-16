969c764b3ce3c4273d7804a3c0a452c8b344ed99
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-16 20:27:20 -0300

    form method returning modelAndView object now

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
@@ -5,8 +5,10 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eduardoportfolio.weblibrary.dao.ProductDao;
+import com.eduardoportfolio.weblibrary.models.BookType;
 import com.eduardoportfolio.weblibrary.models.Product;
 
 @Controller
@@ -16,12 +18,14 @@ public class ProductsController {
 	@Autowired
 	private ProductDao productDao;
 	
-	@RequestMapping("products/form")
-	public String form(){
-		return "products/book-form";
+	@RequestMapping("form")
+	public ModelAndView form(){
+		ModelAndView modelAndView = new ModelAndView("products/book-form");
+		modelAndView.addObject("types", BookType.values());
+		return modelAndView;
 	}
 	
-	@RequestMapping("/products")
+	@RequestMapping("products")
 	public String save(Product product){
 		System.out.println("Saving product "+ product);
 		productDao.save(product);
