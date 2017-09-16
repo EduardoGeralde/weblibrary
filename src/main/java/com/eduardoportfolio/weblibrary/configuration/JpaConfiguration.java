9667526a33fbfd362a982e8890916bc5f363ff06
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-16 00:03:13 -0300

    Local Container Entity Manager with JpaVendor, DataSource and Properties configured

---
+++ src/main/java/com/eduardoportfolio/weblibrary/configuration/JpaConfiguration.java
@@ -0,0 +1,48 @@
+package com.eduardoportfolio.weblibrary.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+public class JpaConfiguration {
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String [] {"com.eduardoportfolio.weblibrary.models"});
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+		
+		return em;
+	}
+	
+	@Bean
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUsername("root");
+		dataSource.setPassword("password");
+		
+		return dataSource;
+	}
+	
+	public Properties additionalProperties(){
+		
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		
+		return properties;
+	}
+
+}
