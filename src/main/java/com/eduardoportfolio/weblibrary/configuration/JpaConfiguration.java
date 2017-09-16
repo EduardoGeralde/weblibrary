70fd09b72313fa2c6d009b7999c5dc06d6a3f76d
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-16 00:49:03 -0300

    url setted and driver changed

--- src/main/java/com/eduardoportfolio/weblibrary/configuration/JpaConfiguration.java
+++ src/main/java/com/eduardoportfolio/weblibrary/configuration/JpaConfiguration.java
@@ -2,14 +2,19 @@ package com.eduardoportfolio.weblibrary.configuration;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 public class JpaConfiguration {
 	
 	@Bean
@@ -28,7 +33,8 @@ public class JpaConfiguration {
 	@Bean
 	public DataSource dataSource(){
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost/weblibrary");
 		dataSource.setUsername("root");
 		dataSource.setPassword("password");
 		
@@ -44,5 +50,14 @@ public class JpaConfiguration {
 		
 		return properties;
 	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		
+		return transactionManager;
+	}
 
+	
 }
