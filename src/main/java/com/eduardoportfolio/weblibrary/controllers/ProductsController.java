3937fa6c88864d6d0469b3e73c10d181194088dd
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-15 23:43:03 -0300

    form and save actions created

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
@@ -1,12 +1,17 @@
 package com.eduardoportfolio.weblibrary.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.eduardoportfolio.weblibrary.dao.ProductDao;
 import com.eduardoportfolio.weblibrary.models.Product;
 
 @Controller
 public class ProductsController {
+	
+	@Autowired
+	private ProductDao productDao;
 	
 	@RequestMapping("products/form")
 	public String form(){
@@ -16,6 +21,7 @@ public class ProductsController {
 	@RequestMapping("/products")
 	public String save(Product product){
 		System.out.println("Saving product "+ product);
+		productDao.save(product);
 		return "products/ok";
 	}
 	
