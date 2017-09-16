51f842ec9efe1b424542750b83a77946ceafb92d
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-16 00:49:26 -0300

    binding setting

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
@@ -1,5 +1,7 @@
 package com.eduardoportfolio.weblibrary.controllers;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import com.eduardoportfolio.weblibrary.dao.ProductDao;
 import com.eduardoportfolio.weblibrary.models.Product;
 
 @Controller
+@Transactional
 public class ProductsController {
 	
 	@Autowired
