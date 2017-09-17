e89aec023830e1b844d5619635c3804eb507a451
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-16 21:09:35 -0300

    list method created

--- src/main/java/com/eduardoportfolio/weblibrary/dao/ProductDao.java
+++ src/main/java/com/eduardoportfolio/weblibrary/dao/ProductDao.java
@@ -1,5 +1,7 @@
 package com.eduardoportfolio.weblibrary.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -17,6 +19,9 @@ public class ProductDao {
 		manager.persist(product);
 	}
 	
+	public List<Product> list(){
+		return manager.createQuery("select distinct(p) from Product p join fetch p.prices",
+																Product.class).getResultList();
+	}
 	
-
 }
