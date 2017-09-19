eafc28d84c2b82c2613348b5984c5c442efba625
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-19 11:35:12 -0300

    multipartFile added in the save action, and FileSaver class used

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
@@ -1,5 +1,6 @@
 package com.eduardoportfolio.weblibrary.controllers;
 
+import javax.servlet.http.Part;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
@@ -8,10 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eduardoportfolio.weblibrary.dao.ProductDao;
+import com.eduardoportfolio.weblibrary.infra.FileSaver;
 import com.eduardoportfolio.weblibrary.models.BookType;
 import com.eduardoportfolio.weblibrary.models.Product;
 
@@ -22,6 +25,8 @@ public class ProductsController {
 	
 	@Autowired
 	private ProductDao productDao;
+	@Autowired
+	private FileSaver fileSaver;
 	
 	@RequestMapping("form")
 	public ModelAndView form(Product product){
@@ -31,12 +36,16 @@ public class ProductsController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, name="saveProduct")
-	public ModelAndView save(@Valid Product product, 
+	public ModelAndView save(@Valid Product product,
+								MultipartFile summary,
 								BindingResult bindingResult, 
 								RedirectAttributes redirectAttributes){
 		if(bindingResult.hasErrors()){
 			return form(product);
 		}
+		System.out.println(summary.getName()+" ; "+summary.getOriginalFilename());
+		String webPath = fileSaver.write("uploaded-images", summary);
+		product.setSummaryWebPath(webPath);
 		productDao.save(product);
 		//add an object just for the next request, can be accessed with EL ${sucess}
 		//Ex.${sucess} in the view
