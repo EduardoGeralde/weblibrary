9667f627971f130577ca64a7c94c309e7adbafff
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-19 11:33:52 -0300

    MultiPartConfigElement registered in the customizeRegistration overrided method

--- src/main/java/com/eduardoportfolio/weblibrary/configuration/SpringServlet.java
+++ src/main/java/com/eduardoportfolio/weblibrary/configuration/SpringServlet.java
@@ -1,5 +1,8 @@
 package com.eduardoportfolio.weblibrary.configuration;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 
@@ -21,6 +24,16 @@ public class SpringServlet extends AbstractAnnotationConfigDispatcherServletInit
 	protected String[] getServletMappings() {
 		// TODO Auto-generated method stub
 		return new String [] {"/"};
+	}
+	
+	@Override
+	//Upload treatment. We can choose the temporary storage location (while receiving file), 
+	//maximum size file, maximum request size, and so on. It receives a Dynamic Object that allow 
+	//us register our configuration object of MultipartConfigElement type. We use in parallel with 
+	//MultipartRsolver in the AppWebConfiguration class, to work with files.
+	//With the parameter "", indicates that the web server will decide which location to use
+	protected void customizeRegistration(Dynamic registration){
+		registration.setMultipartConfig(new MultipartConfigElement(""));
 	}
 
 }
