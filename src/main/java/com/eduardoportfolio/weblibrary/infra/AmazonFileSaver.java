7fbb7d5eccc5ddfae96a6e554872043bfc7b735c
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-19 23:42:36 -0300

    improvement

--- src/main/java/com/eduardoportfolio/weblibrary/infra/AmazonFileSaver.java
+++ src/main/java/com/eduardoportfolio/weblibrary/infra/AmazonFileSaver.java
@@ -2,25 +2,23 @@ package com.eduardoportfolio.weblibrary.infra;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.AmazonClientException;
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.S3ClientOptions;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @Component
 public class AmazonFileSaver {
 	
+	@Autowired
+	private AmazonS3Client s3;
 	
-	public String writer(String baseFolder, MultipartFile multipartFile){
-		//The AmazonS3Client type object encapsulate all the logic and the communication protocol
-		AmazonS3Client s3= client ();
+	public String write(String baseFolder, MultipartFile multipartFile){
 		try{
+			//The AmazonS3Client type object encapsulate all the logic and the communication protocol
 			//We have only to pass the parameters, first the remote folder where we will save the
 			//file (bucket), second is the file name, third is some InputStream implementation
 			//thats really represent the file and fourth some extra informations like, expire file
@@ -28,18 +26,10 @@ public class AmazonFileSaver {
 			s3.putObject("weblibrary", multipartFile.getOriginalFilename(),
 										multipartFile.getInputStream(),
 										new ObjectMetadata());
-			return "https://s3.amazonaws.com/weblibrary/" + multipartFile.getOriginalFilename();
-		} catch (AmazonClientException | IOException e){
+			return "http://localhost:9444/s3/weblibrary/" + multipartFile.getOriginalFilename() + 
+																				"?noAuth=true";
+		} catch (AmazonClientException|IOException e){
 			throw new RuntimeException (e);
 		}
-	}
-	
-	//In any Amazon integration, we always have to create a object that has the access credentials 
-	private AmazonS3Client client(){
-		AWSCredentials credentials = new BasicAWSCredentials("example","example");
-		AmazonS3Client newClient = new AmazonS3Client(credentials,new ClientConfiguration());
-		newClient.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(true));
-		
-		return newClient;
 	}
 }
