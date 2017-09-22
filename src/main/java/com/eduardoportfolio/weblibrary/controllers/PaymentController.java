30b57ccd218216ff338e30cf894d6a9888306942
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-22 17:11:54 -0300

    payment integration with heroku API

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/PaymentController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/PaymentController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
+import com.eduardoportfolio.weblibrary.models.PaymentData;
 import com.eduardoportfolio.weblibrary.models.ShoppingCart;
 
 @Controller
@@ -15,13 +18,20 @@ public class PaymentController {
 
 	@Autowired
 	ShoppingCart shoppingCart;
+	@Autowired
+	RestTemplate restTemplate;
 	
 	@RequestMapping(value="checkout", method=RequestMethod.POST)
 	public String checkout(){
+		
 		BigDecimal total = shoppingCart.getTotal();
 		
-		//Integration Code
-		
-		return "redirect:/success";
+		String uriToPay = "http://book-payment.herokuapp.com/payment";
+		try{
+			String response = restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
+			return "payment/payment-success";
+		} catch (HttpClientErrorException exception){
+			return "payment/payment-error";
+		}
 	}
 }
