9d779067a21e9fcdfd27e03f00715b4251194b06
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-22 00:16:34 -0300

    request scoped

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/ShoppingCartController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/ShoppingCartController.java
@@ -1,11 +1,13 @@
 package com.eduardoportfolio.weblibrary.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.eduardoportfolio.weblibrary.dao.ProductDao;
@@ -16,6 +18,7 @@ import com.eduardoportfolio.weblibrary.models.ShoppingItem;
 
 @Controller
 @RequestMapping("/shopping")
+@Scope(value=WebApplicationContext.SCOPE_REQUEST)
 public class ShoppingCartController {
 
 	@Autowired
