08bbff9c4ca2d583c1a6ad8b53bab8389ccaa40a
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-21 22:45:24 -0300

    parameter name changed

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/ShoppingCartController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/ShoppingCartController.java
@@ -1,8 +1,11 @@
 package com.eduardoportfolio.weblibrary.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.eduardoportfolio.weblibrary.dao.ProductDao;
@@ -15,13 +18,14 @@ import com.eduardoportfolio.weblibrary.models.ShoppingItem;
 @RequestMapping("/shopping")
 public class ShoppingCartController {
 
+	@Autowired
 	private ProductDao productDao;
-	
+	@Autowired
 	private ShoppingCart shoppingCart;
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView add(Integer prodId, BookType bookType){
-		ShoppingItem item = createItem(prodId, bookType);
+	public ModelAndView add(Integer productId, @RequestParam BookType bookType){
+		ShoppingItem item = createItem(productId, bookType);
 		shoppingCart.add(item);
 		return new ModelAndView("redirect:/products");
 	}
@@ -32,5 +36,14 @@ public class ShoppingCartController {
 		return item;
 	}
 	
+	@RequestMapping(method=RequestMethod.GET)
+	public String items(){
+		return "shoppingCart/items";
+	}
 	
+	@RequestMapping(method=RequestMethod.POST, value="/{productId}")
+	public String remove(@PathVariable("productId") Integer productId, BookType bookType){
+		shoppingCart.remove(createItem(productId, bookType));
+		return "redirect:/shopping";
+	}
 }
