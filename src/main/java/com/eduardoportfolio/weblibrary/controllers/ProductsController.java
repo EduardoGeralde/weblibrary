5abcd68ad3b7cdea2815b50523df12e4e6869f47
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-23 00:43:03 -0300

    list method cached

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/ProductsController.java
@@ -4,6 +4,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,7 +54,9 @@ public class ProductsController {
 		return new ModelAndView("redirect:/products");
 	}
 	
+	
 	@RequestMapping(method=RequestMethod.GET)
+	@Cacheable (value="books")
 	public ModelAndView list(){
 		ModelAndView modelAndView = new ModelAndView("products/list");
 		modelAndView.addObject("products", productDao.list());
@@ -70,5 +73,4 @@ public class ProductsController {
 		modelAndView.addObject("product", product);
 		return modelAndView;
 	}
-
 }
