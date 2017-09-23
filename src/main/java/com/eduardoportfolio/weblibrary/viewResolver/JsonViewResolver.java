4863523e4620b49378b350d29860a991e56f73ff
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-23 19:52:03 -0300

    key setted for products

--- src/main/java/com/eduardoportfolio/weblibrary/viewResolver/JsonViewResolver.java
+++ src/main/java/com/eduardoportfolio/weblibrary/viewResolver/JsonViewResolver.java
@@ -9,10 +9,13 @@ import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 public class JsonViewResolver implements ViewResolver {
 
 	@Override
+	//Iterates for all keys in the ModelAndView and generates the Json for each found object
 	public View resolveViewName(String viewName, Locale locale) throws Exception {
 		//We don't need to give a parameter with the view name, because we won't redirect to any file 
 		MappingJackson2JsonView view = new MappingJackson2JsonView();
 		view.setPrettyPrint(true);
+		//We can define only which key in the modelAndView have to generate Json
+		view.setModelKey("products");
 		return view;
 	}
 
