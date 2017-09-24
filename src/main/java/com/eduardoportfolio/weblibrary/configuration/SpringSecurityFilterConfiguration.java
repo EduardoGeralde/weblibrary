bb3525bb76cda200a43c519c9ea287f5285c4c01
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-23 23:57:33 -0300

    filter security configuration class created

---
+++ src/main/java/com/eduardoportfolio/weblibrary/configuration/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,9 @@
+package com.eduardoportfolio.weblibrary.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//This class inherits all the work to register filters. Ok, filter configured, ready to work
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer{
+
+	
+}
