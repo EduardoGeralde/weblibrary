e398a1c2f1f6b68c7de7ecf626ab0b620577ed9d
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-24 23:41:48 -0300

    Role class created

---
+++ src/main/java/com/eduardoportfolio/weblibrary/models/Role.java
@@ -0,0 +1,19 @@
+package com.eduardoportfolio.weblibrary.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+public class Role implements GrantedAuthority {
+	
+	@Id
+	private String name;
+	
+	@Override
+	public String getAuthority() {
+		return name;
+	}
+
+}
