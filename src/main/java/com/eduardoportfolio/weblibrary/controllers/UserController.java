19d3e49bcd959edc20d0778b88b87e8f139d61ce
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-30 00:07:12 -0300

    initBinder for RolePropertyEditor

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/UserController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/UserController.java
@@ -4,27 +4,34 @@ package com.eduardoportfolio.weblibrary.controllers;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.amazonaws.services.identitymanagement.model.Role;
 import com.eduardoportfolio.weblibrary.dao.RoleDao;
 import com.eduardoportfolio.weblibrary.dao.UserDao;
+import com.eduardoportfolio.weblibrary.infra.RolePropertyEditor;
+import com.eduardoportfolio.weblibrary.models.Role;
 import com.eduardoportfolio.weblibrary.models.User;
 
 @Controller
 @Transactional
 @RequestMapping("/register")
-//@Scope(value=WebApplicationContext.SCOPE_REQUEST)
+@Scope(WebApplicationContext.SCOPE_REQUEST)
 public class UserController {
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		binder.registerCustomEditor(Role.class, new RolePropertyEditor(roleDao));
+	}
 	
 	@Autowired
 	UserDao userDao;
@@ -32,16 +39,15 @@ public class UserController {
 	RoleDao roleDao;
 
 	@RequestMapping("userForm")
-	public ModelAndView userForm(User user){
+	public ModelAndView userForm(User user) {
 		ModelAndView modelAndView = new ModelAndView("user/userForm");
 		modelAndView.addObject("roleList", roleDao.list());
 		return modelAndView;
 	}
 	
-	
 	@RequestMapping (value="saveUser", method=RequestMethod.POST, name="saveUser")
 	public ModelAndView saveUser(@Valid User user, BindingResult bindingResult, 
-															RedirectAttributes redirectAttributes){
+													RedirectAttributes redirectAttributes) {
 		
 		if (bindingResult.hasErrors()){
 			return userForm(user);
@@ -51,5 +57,4 @@ public class UserController {
 		redirectAttributes.addFlashAttribute("success", "User successfully registered");
 		return new ModelAndView("redirect:/register/userForm");
 	}
-	
 }
