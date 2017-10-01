53e3be8534d2eda1db7a57d699c9b3a22a4d5885
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-01 15:17:58 -0300

    comment added

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/AuthController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthController {
-	
+	//Binding in the SecurityConfiguration
 	@RequestMapping("/login")
 	public String loginPage(){
 		return "auth/login";
