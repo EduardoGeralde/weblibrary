19f6b6533298aac4971f17eaa276f121fb7a7402
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-01 20:52:34 -0300

    testing

--- src/test/java/com/eduardoportfolio/weblibrary/dao/ProductDaoTest.java
+++ src/test/java/com/eduardoportfolio/weblibrary/dao/ProductDaoTest.java
@@ -1,4 +1,4 @@
-package com.eduardoportfolio.welibrary.dao;
+package com.eduardoportfolio.weblibrary.dao;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -9,18 +9,21 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.eduardoportfolio.weblibrary.builder.ProductBuilder;
+import com.eduardoportfolio.weblibrary.conf.DataSourceConfigurationTest;
 import com.eduardoportfolio.weblibrary.configuration.JpaConfiguration;
-import com.eduardoportfolio.weblibrary.dao.ProductDao;
 import com.eduardoportfolio.weblibrary.models.BookType;
 import com.eduardoportfolio.weblibrary.models.Product;
 
 //Its provided so that frameworks can be notified of the phases of the test execution
 @RunWith(SpringJUnit4ClassRunner.class)
 //Tells which classes has to be load
-@ContextConfiguration(classes = {ProductDao.class, JpaConfiguration.class})
+@ContextConfiguration(classes = {ProductDao.class, JpaConfiguration.class, DataSourceConfigurationTest.class})
+@ActiveProfiles("test")
 public class ProductDaoTest {
 
 	@Autowired
@@ -43,7 +46,7 @@ public class ProductDaoTest {
 		ebooks.stream().forEach(productDao::save);
 		
 		BigDecimal value = productDao.sumPricesPerType(BookType.PRINTED);
-		Assert.assertEquals(new BigDecimal(50).setScale(2), value);	
+		Assert.assertEquals(new BigDecimal(50).setScale(2), value);
 	}
 
 }
