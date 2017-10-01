f6386b44413e83d524efc911d8567de374529ad3
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-01 20:53:16 -0300

    new datasource for testing

---
+++ src/test/java/com/eduardoportfolio/weblibrary/conf/DataSourceConfigurationTest.java
@@ -0,0 +1,21 @@
+package com.eduardoportfolio.weblibrary.conf;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class DataSourceConfigurationTest {
+
+	@Bean
+	@Profile("test")
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost/weblibrary_test");
+		dataSource.setUsername("root");
+		dataSource.setPassword("password");
+		return dataSource;
+	}
+}
