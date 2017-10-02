cf9eb7f4c20ee49016d8f27f5d7c3def619b69cf
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-02 20:52:01 -0300

    email sender added

--- src/main/java/com/eduardoportfolio/weblibrary/controllers/PaymentController.java
+++ src/main/java/com/eduardoportfolio/weblibrary/controllers/PaymentController.java
@@ -4,14 +4,19 @@ import java.math.BigDecimal;
 import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eduardoportfolio.weblibrary.models.PaymentData;
 import com.eduardoportfolio.weblibrary.models.ShoppingCart;
+import com.eduardoportfolio.weblibrary.models.User;
 
 @Controller
 @RequestMapping("/payment")
@@ -21,10 +26,13 @@ public class PaymentController {
 	ShoppingCart shoppingCart;
 	@Autowired
 	RestTemplate restTemplate;
+	@Autowired
+	private MailSender mailSender;
 	
 	@RequestMapping(value="checkout", method=RequestMethod.POST)
-	//Analog to Runnable, but allow us give a return
-	public Callable<String> checkout(){
+	//Analog to Runnable, but allow us give a return. @AuthenticationPrincipal, allow receive the
+	//user logged as a parameter
+	public Callable<ModelAndView> checkout(@AuthenticationPrincipal User user){
 		
 		return() -> {
 		
@@ -33,11 +41,21 @@ public class PaymentController {
 			String uriToPay = "http://book-payment.herokuapp.com/payment";
 			try{
 				String response = restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
-				return "payment/payment-success";
+				sendNewPurchaseMail(user);
+				return new ModelAndView("payment/payment-success");
 			} catch (HttpClientErrorException exception){
-				return "payment/payment-error";
+				return new ModelAndView("payment/payment-error");
 			}
 		};
+	}
+	
+	private void sendNewPurchaseMail(User user){
+		SimpleMailMessage mail = new SimpleMailMessage();
+		mail.setFrom("edardogeralde83@gmail.com");
+		mail.setTo(user.getLogin());
+		mail.setSubject("New Purchase");
+		mail.setText("Mail body");
+		mailSender.send(mail);
 	}
 	
 }
